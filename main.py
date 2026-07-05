0be47df460d5af459a586193b2bc98a07fa9c0f2
gunqio <vssvnj180@gmail.com>
2026-07-06 00:31:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ except ImportError:
 RENEW_URLS = [
     "https://host2play.gratis/server/renew?i=f9ba3248-9756-48ea-aabd-a5d8ecb940e9",
     "https://host2play.gratis/server/renew?i=cf93abbb-7460-459b-8b60-5e46c9855a16",
-     添加更多链接
+  
 ]
 
 MAX_CAPTCHA = 3
