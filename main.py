1d6815c61a46016a51eaa3f8dd82cf22f044ba82
bibibile <38691327+bibibile@users.noreply.github.com>
2026-07-29 20:40:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=9fc09769-3ed5-4159-a73b-d0bcb0f43599",
-    "https://host2play.gratis/server/renew?i=0765de32-cbf8-4ccf-8965-6777d3a90d9f",
+    "https://host2play.gratis/server/renew?i=e6ff9e33-0625-4e9f-8082-7238fc936c64",
+    "https://host2play.gratis/server/renew?i=26a99443-c95c-4504-bd60-f3fdcd0cf09f",
   
 ]
 
